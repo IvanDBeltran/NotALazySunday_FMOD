ab3a7a164f30346b0e5a99c8985e6750a5f128da
IvanDBeltran <ivan.daniel.beltran@gmail.com>
2024-02-17 11:32:11 -0500

    First commit

---
+++ fmod_studio_guids.cs
@@ -0,0 +1,28 @@
+/*
+    fmod_studio_guids.cs - FMOD Studio API
+
+    Generated GUIDs for project 'NotALazySunday_FMOD.fspro'
+*/
+
+namespace FSPRO
+{
+    public class Event
+    {
+        public static readonly FMOD.GUID Music_MainTitleMX = new FMOD.GUID { Data1 = 1304199600, Data2 = 1307272827, Data3 = 1346105500, Data4 = 426698670 };
+        public static readonly FMOD.GUID Player_Footsteps = new FMOD.GUID { Data1 = -326314589, Data2 = 1166331326, Data3 = 706357176, Data4 = 931117316 };
+    }
+
+    public class Bus
+    {
+        public static readonly FMOD.GUID Master_Bus = new FMOD.GUID { Data1 = 1824983211, Data2 = 1307391562, Data3 = -1940884821, Data4 = 219395801 };
+        public static readonly FMOD.GUID Reverb = new FMOD.GUID { Data1 = -1534746915, Data2 = 1282584552, Data3 = -474328695, Data4 = 750784257 };
+    }
+
+    public class Bank
+    {
+        public static readonly FMOD.GUID Master = new FMOD.GUID { Data1 = 664265175, Data2 = 1090854864, Data3 = 1941015992, Data4 = 105956843 };
+        public static readonly FMOD.GUID Player = new FMOD.GUID { Data1 = -267379798, Data2 = 1218630458, Data3 = -117367159, Data4 = 16467586 };
+    }
+
+}
+
